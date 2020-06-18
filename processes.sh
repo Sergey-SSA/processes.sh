e076dc9885d2f7a3f887201d4f88e8b21b598f8f
sur <ssa_karagay@mail.ru>
2020-06-18 22:19:37 +0300

    add_all

---
+++ processes.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+prc=`ls /proc | grep [0-9] | sort -n`
+for i in $prc
+    do
+	awk -F" " '{print "PID:"$1,"PPID:"$4,"STATUS:"$3,"NICE:"$19,"PROCESS:"$2}' /proc/$i/stat | column -t
+    done
